64392ab58e92d2d3514cebb56d5c99c026a75cf6
andrusrem <andrusremets@gmail.com>
2023-10-20 15:52:27 +0300

    Co-authored-by: Retro <dankmolot@users.noreply.github.com>

--- KooliProjekt/Data/ApplicationDbContext.cs
+++ KooliProjekt/Data/ApplicationDbContext.cs
@@ -14,5 +14,6 @@ namespace KooliProjekt.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Order> Orders {get;set;}
+        
     }
 }
