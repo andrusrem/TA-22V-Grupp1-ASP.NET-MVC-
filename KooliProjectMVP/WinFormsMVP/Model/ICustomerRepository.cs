54b1c2ac65ad1c3d12b20d6e1e89656d4b89ca96
andrusrem <andrusremets@gmail.com>
2024-05-09 12:55:57 +0300

    MVP project, where we can see list of users

---
+++ KooliProjectMVP/WinFormsMVP/Model/ICustomerRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsMVP.Model
+{
+    public interface ICustomerRepository
+    {
+        Task<IList<Customer>> List();
+        Task<Customer> Get(string id);
+
+        Task<List<Customer>> GetCustomers();
+        Task<Customer> GetCustomerApi(string id);
+    }
+}
---
+++ KooliProjectMVP/WinFormsMVP/Presenter/CustomerPresenter.cs
@@ -0,0 +1,55 @@
+﻿using WinFormsMVP.View;
+using WinFormsMVP.Model;
+using System.Linq;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Text;
+using System;
+using System.Threading.Tasks;
+
+namespace WinFormsMVP.Presenter
+{
+    public class CustomerPresenter
+    {
+        private readonly ICustomerView _view;
+        private readonly ICustomerRepository _repository;
+
+        public CustomerPresenter(ICustomerView view, ICustomerRepository repository)
+        {
+            _view = view;
+            view.Presenter = this;
+            _repository = repository;
+
+            UpdateCustomerListView();
+            
+        }
+
+        private void UpdateCustomerListView()
+        {
+            var task = Task.Run(async () => await _repository.GetCustomers());
+            task.Wait();
+            var list = task.Result;
+            int selectedCustomer = _view.SelectedCustomer >= 0 ? _view.SelectedCustomer : 0;
+            _view.CustomerList = list;
+            _view.SelectedCustomer = selectedCustomer;
+            
+
+        }
+
+        public void UpdateCustomerView(string p)
+        {
+            
+            var customerTask = Task.Run(async () => await _repository.GetCustomerApi(p));
+            customerTask.Wait();
+            var customer = customerTask.Result;
+
+            
+            _view.CustomerName = customer.Email;
+            _view.Address = customer.Address;
+            _view.Phone = customer.Phone;
+            
+            
+        }
+    }
+}
