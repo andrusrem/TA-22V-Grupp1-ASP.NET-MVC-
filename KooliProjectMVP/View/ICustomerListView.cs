8b96facc5babe99171d68d6420fb5a1b7c351e32
Gunnar Peipman <gpeipman@hotmail.com>
2024-05-09 13:05:56 +0300

    MVP projekt õigesse kohta

---
+++ KooliProjectMVP/View/ICustomerListView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinFormsMVP.Model;
+
+namespace WinFormsMVP.View
+{
+    public interface ICustomerListView
+    {
+        IList<Customer> CustomersList { get; set; }
+        Presenter.CustomerPresenter Presenter { get; set; }
+    }
+}
